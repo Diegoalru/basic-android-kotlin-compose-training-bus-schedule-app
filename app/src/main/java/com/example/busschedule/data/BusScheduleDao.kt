af26c67dd6e6df32375e2fc0fe7f1c8849e9132a
Diego Alonso Rubí Salas <43016665+Diegoalru@users.noreply.github.com>
2024-09-01 19:25:18 -0600

    Order schedules by stop time.

--- app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
@@ -9,6 +9,6 @@ interface BusScheduleDao {
     @Query("SELECT * FROM Schedule ORDER BY arrival_time ASC")
     fun getAll(): Flow<List<BusSchedule>>
 
-    @Query("SELECT * FROM Schedule WHERE stop_name = :stopName")
+    @Query("SELECT * FROM Schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
     fun getByStopName(stopName: String): Flow<List<BusSchedule>>
 }
